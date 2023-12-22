f9e21a3d93467c8f26f60706852bf178401ec689
Wi-ame <119756822+Wi-ame@users.noreply.github.com>
2023-12-22 19:54:20 +0100

    Ajoutez des fichiers projet.

---
+++ vente_en_ligne/Models/Utilisateur.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vente_en_ligne.Models
+{
+    public class Utilisateur
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Nom { get; set; }
+
+        [Required]
+        public string Prenom { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Veuillez saisir une adresse e-mail valide.")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(\+[0-9]{1,3})?[0-9]{10,15}$", ErrorMessage = "Veuillez saisir un numéro de téléphone valide.")]
+        public string Tel { get; set; }
+    }
+}
